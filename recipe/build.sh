12d66ca3f00b66551bb2922cacd46d69ccf1aeaa
Isuru Fernando <isuruf@gmail.com>
2021-08-10 14:17:27 +0530

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,13 @@
 
 sed -i.bak "s/CODESIGN=.*/CODESIGN=true/g" Makefile.build
 export LIBRARY_PATH=$PREFIX/lib
+export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
 
 ./configure -prefix=$PREFIX
 
 chmod +w config/coq_config.ml
 sed "s@\"${OCAML_PREFIX}/@(Sys.getenv \"OCAML_PREFIX\") ^ \"/@g" config/coq_config.ml > config/coq_config.ml.bak
 sed "s@\"file:/${OCAML_PREFIX}/@\"file:/\" ^ (Sys.getenv \"OCAML_PREFIX\") ^ \"/@g" config/coq_config.ml.bak > config/coq_config.ml
+cat config/coq_config.ml
 make -j${CPU_COUNT}
 make install
