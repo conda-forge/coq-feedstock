7194314f76f39aa708d2d62739736cd5e6a546aa
Isuru Fernando <isuruf@gmail.com>
2021-08-10 15:15:36 +0530

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,5 +10,5 @@ chmod +w config/coq_config.ml
 sed "s@\"${OCAML_PREFIX}/@(Sys.getenv \"OCAML_PREFIX\") ^ \"/@g" config/coq_config.ml > config/coq_config.ml.bak
 sed "s@\"file:/${OCAML_PREFIX}/@\"file:/\" ^ (Sys.getenv \"OCAML_PREFIX\") ^ \"/@g" config/coq_config.ml.bak > config/coq_config.ml
 cat config/coq_config.ml
-make -j${CPU_COUNT}
+make -j${CPU_COUNT} || ($OTOOL -l $PWD/bin/ocamllibdep && false)
 make install
