91086b89296ccefe064e3c61545cb642f853cf52
Isuru Fernando <isuruf@gmail.com>
2021-08-10 17:51:59 +0530

    add rpath manually

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,5 +10,12 @@ chmod +w config/coq_config.ml
 sed "s@\"${OCAML_PREFIX}/@(Sys.getenv \"OCAML_PREFIX\") ^ \"/@g" config/coq_config.ml > config/coq_config.ml.bak
 sed "s@\"file:/${OCAML_PREFIX}/@\"file:/\" ^ (Sys.getenv \"OCAML_PREFIX\") ^ \"/@g" config/coq_config.ml.bak > config/coq_config.ml
 cat config/coq_config.ml
-make -j${CPU_COUNT} || ($OTOOL -l $PWD/bin/ocamllibdep && false)
+
+if [[ "$target_platform" == osx-64 ]]; then
+  make -j${CPU_COUNT} || true
+  $INSTALL_NAME_TOOL -add_rpath $PREFIX/lib $PWD/bin/ocamllibdep
+  $OTOOL -l $PWD/bin/ocamllibdep
+fi
+
+make -j${CPU_COUNT}
 make install
