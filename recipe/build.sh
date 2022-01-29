b9ca607ba7c52b96e81483656c4b60916908fcb8
Isuru Fernando <isuruf@gmail.com>
2022-01-29 14:20:21 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sed -i.bak "s/CODESIGN=.*/CODESIGN=true/g" Makefile.build
+sed -i.bak 's/$(LINKMETADATA)/$(LINKMETADATA) '"-cclib -L$PREFIX/lib -cclib -Wl,-rpath,$PREFIX/lib" Makefile.build
 export LIBRARY_PATH=$PREFIX/lib
 export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
 
@@ -10,12 +11,6 @@ chmod +w config/coq_config.ml
 sed "s@\"${OCAML_PREFIX}/@(Sys.getenv \"OCAML_PREFIX\") ^ \"/@g" config/coq_config.ml > config/coq_config.ml.bak
 sed "s@\"file:/${OCAML_PREFIX}/@\"file:/\" ^ (Sys.getenv \"OCAML_PREFIX\") ^ \"/@g" config/coq_config.ml.bak > config/coq_config.ml
 cat config/coq_config.ml
-
-if [[ "$target_platform" == osx-64 ]]; then
-  make -j${CPU_COUNT} || true
-  $INSTALL_NAME_TOOL -add_rpath $PREFIX/lib $PWD/bin/ocamllibdep
-  $OTOOL -l $PWD/bin/ocamllibdep
-fi
 
 make -j${CPU_COUNT}
 make install
